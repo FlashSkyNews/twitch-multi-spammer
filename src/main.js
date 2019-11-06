d3d6cac147c4170977b281cc9775b1a5a904939d
teyd <dankdankdank321@gmail.com>
2019-11-06 16:34:21 +0100

    multiple accounts supported

--- src/main.js
+++ src/main.js
@@ -2,13 +2,19 @@ import Spammer from "./spammer";
 import { sleep } from "./helper";
 import config from "../config.json";
 
-const spammer = new Spammer(config.username, config.oauth);
-spammer.joinChannel(config.targetChannel);
+let spammers = [];
+for (const account of config.accounts) {
+    const spammer = new Spammer(account.username, account.oauth);
+    spammer.joinChannel(config.targetChannel);
+    spammers.push(spammer);
+}
 
 (async() => {
     // eslint-disable-next-line no-constant-condition
     while (true) {
-        spammer.writeMessage(config.targetChannel, config.message);
+        for (const spammer of spammers) {
+            spammer.writeMessage(config.targetChannel, config.message);
+        }
         await sleep(config.timeToSleep);
     }
 })();
